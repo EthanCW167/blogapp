155f6fc4e23c26357ba2ba3cc1d5a5a4f289bed8
Ethan Wilson <E.Wilson17@newcastle.ac.uk>
2023-09-28 15:54:44 +0100

    Added user authentication

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -15,7 +15,7 @@
     @vite('resources/css/app.css')
 </head>
 <body class="w-full h-full bg-gray-100">
-    <div class="w-4/5 mx-auto">
+    <div class="w-4/5 mx-auto pb-10">
         <div class="text-center pt-20">
             <h1 class="text-3xl text-gray-700">
                 All Articles
@@ -23,12 +23,15 @@
             <hr class="border border-1 border-gray-300 mt-10">
         </div>
 
+        @if (Auth::user())
+
         <div class="py-10 sm:py-20">
             <a class="primary-btn inline text-base sm:text-xl bg-green-500 py-4 px-4 shadow-xl rounded-full transition-all hover:bg-green-400"
                href="{{ route('blog.create') }}">
                 New Article
             </a>
         </div>
+        @endif
     </div>
 
     @if (session()->has('message'))
@@ -64,18 +67,21 @@
                         </a>
                     on {{ $post->updated_at->format('d/m/Y') }}
                 </span>
+                @if (Auth::user())
                 <div>
-                <a href="{{ route('blog.edit', $post->id) }}" class="bloack italic text-green-500 border-b-1 border-green-400">
-                    Edit
-                </a>
-                <form action="{{ route('blog.destroy', $post->id) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button class="pt-3 text-red-500 pr-3" type="submit">
-                        Delete
-                    </button>
-                </form>
-            </div>
+                    <a href="{{ route('blog.edit', $post->id) }}" class="bloack italic text-green-500 border-b-1 border-green-400">
+                        Edit
+                    </a>
+                    <form action="{{ route('blog.destroy', $post->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="pt-3 text-red-500 pr-3" type="submit">
+                            Delete
+                        </button>
+                    </form>
+                    </div>
+                @endif
+
             </div>
         </div>
     </div>
